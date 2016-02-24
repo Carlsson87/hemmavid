f840faebb62f3967dd540248d8275c3bd9497ffa
Daniel Carlsson <daniel.carlsson@hemsida24.se>
2016-02-24 20:55:18 +0100

    Clean up the budget view

--- resources/views/budget.blade.php
+++ resources/views/budget.blade.php
@@ -2,76 +2,75 @@
 
 @section('content')
 <div class="row">
-    <div class="col-xs-12 col-sm-8">
-        <div class="panel panel-default">
-            <div class="panel-heading clearfix">
-                <h3 class="panel-title">
-                    Budget
-                </h3>
-            </div>
-            <div class="panel-body">
-                @foreach($categories as $category)
-                    <div class="form-group" id="progress-{{ $category->id }}">
-                        <label>{{ $category->name }}</label>
-                        <label class="pull-right">{{ $category->budget }}kr</label>
-                        <div class="progress">
+    <div class="col-xs-12">
+        @foreach($categories as $category)
+            <div class="panel panel-default category-modal-toggle" data-category="{{ $category->id }}" id="category-{{ $category->id }}" style="cursor: pointer;">
+                <div class="panel-body">
+                    <h3 class="panel-title">
+                        {{ $category->name }}
+                        <span class="pull-right">{{ $category->budget }}kr</span>
+                    </h3>
+                    <div class="form-group" id="progress-{{ $category->id }}" style="margin-bottom: 0;">
+                        <div class="progress" style="margin-bottom: 0;">
                             <div class="progress-bar progress-bar-success" style="line-height: 16px; width: {{ $category->current_percentage }}%;" role="progressbar" aria-valuenow="{{ $category->current_total }}" aria-valuemin="0" aria-valuemax="{{ $category->budget }}">
     {{ $category->current_total }}kr
 </div>
                         </div>
                     </div>
-                @endforeach
-            </div>
-            <div class="panel-footer clearfix">
-                <button class="pull-right btn btn-primary" type="button" data-toggle="modal" data-target="#add-category-modal" style="line-height: 1">Ny kategori</button>
-            </div>
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-4">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">
-                    Lägg till utgift
-                </h3>
-            </div>
-            <div class="panel-body">
-                <div id="form-add-expense">
-                  <div class="form-group">
-                    <label class="control-label">Beskrivning</label>
-                    <input name="description" class="form-control input-sm"/>
-                  </div>
-                  <div class="form-group">
-                    <label class="control-label">Kategori</label>
-                    <select name="category_id" class="form-control input-sm">
-                        @foreach($categories as $category)
-                            <option value="{{ $category->id }}">{{ $category->name }}</option>
-                        @endforeach
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label class="control-label">Kostnad</label>
-                    <div class="input-group">
-                      <input name="cost" type="number" class="form-control input-sm"/>
-                      <span class="input-group-addon">kr</span>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label class="control-label">Datum</label>
-                    <input name="date" value="{{ date('Y-m-d') }}" type="date" class="form-control input-sm"/>
-                  </div>
-                  <div class="form-group text-right">
-                    <button class="btn btn-block btn-success">Spara</button> 
-                  </div>
                 </div>
+            </div>
+        @endforeach
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <button class="btn btn-block btn-link" type="button" data-toggle="modal" data-target="#add-category-modal">Ny kategori</button>
             </div>
         </div>
     </div>
 </div>
-<div class="modal fade" id="add-category-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
-  <div class="modal-dialog" role="document">
+
+<! -- EXPENSE MODAL -->
+<div class="modal fade" id="add-expense-modal">
+  <div class="modal-dialog modal-sm" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <button type="button" class="close" data-dismiss="modal" aria-label="close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Ny utgift</h4>
+      </div>
+      <div class="modal-body">
+        <div id="form-add-expense">
+        <input type="hidden" name="category_id">
+          <div class="form-group">
+            <label class="control-label">Beskrivning</label>
+            <input name="description" class="form-control input-sm"/>
+          </div>
+          <div class="form-group">
+            <label class="control-label">Kostnad</label>
+            <div class="input-group">
+              <input name="cost" type="number" class="form-control input-sm"/>
+              <span class="input-group-addon">kr</span>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label">Datum</label>
+            <input name="date" value="{{ date('Y-m-d') }}" type="date" class="form-control input-sm"/>
+          </div>
+          <div class="form-group text-right">
+            <button class="btn btn-block btn-success">Spara</button> 
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</div>
+
+<! -- CATEGORY MODAL -->
+<div class="modal fade" id="add-category-modal">
+  <div class="modal-dialog modal-sm" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="close"><span aria-hidden="true">&times;</span></button>
         <h4 class="modal-title">Ny kategori</h4>
       </div>
     <form action="/categories" method="post">
@@ -109,10 +108,15 @@
             cost: $form.find("[name='cost']").val(),
             date: $form.find("[name='date']").val()
         };
-            
+        $('#add-expense-modal').modal('hide');
+        $form.find('input').val("");    
         $.post('/categories/' + category_id + '/expenses', data, function(res) {
             $('#progress-' + res.id + ' .progress-bar').css({ width: res.current_percentage + '%' });
             $('#progress-' + res.id + ' .progress-bar').text(res.current_total + 'kr');
         });
+    });
+    $('.category-modal-toggle').click(function() {
+        $("#form-add-expense [name='category_id']").val($(this).data('category'));
+        $('#add-expense-modal').modal('show');
     });
 @stop
